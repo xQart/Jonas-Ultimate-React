f9fb20c30f761597f55dd3f534d94c71499ead13
Angel Jimenez <angelgabrieljs09@gmail.com>
2024-05-30 19:44:22 -0500

    Boton reusable

--- steps/src/App-v1.jsx
+++ steps/src/App-v1.jsx
@@ -41,32 +41,23 @@ function App() {
             Step {step}:{messages[step - 1]}
           </p>
           <div className="buttons">
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              <span>👈</span> Previous
-            </Button>
+              Previous
+            </button>
 
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
 
